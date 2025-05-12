d57e1a5689cb369679b66a9cd1cbd13e5806e586
Erdem Kaya <kerdem@live.com>
2025-05-12 13:15:00 +0200

    Add project files.

---
+++ Presentation/Controllers/SingUpController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models;
+
+namespace Presentation.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class SingUpController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult SingUp(SignUpModel model)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+       
+
+    }
+}
---
+++ Presentation/Services/AuthService.cs
@@ -0,0 +1,27 @@
+﻿using Presentation.Models;
+using System.Reflection;
+
+namespace Presentation.Services;
+
+public class AuthService
+{
+
+    public async Task<bool> AlreadyExistsAsync(string email)
+    {
+        using var http = new HttpClient();
+        var response = await http.GetFromJsonAsync<AccountServiceResult>($"https://localhost:7274/api/accounts?email={email}").Result;
+        return response.Succeeded;
+    }
+
+    public async Task<AuthServiceResult> SignUp(SignUpModel model)
+    {
+        var exists = await AlreadyExistsAsync(model.Email);
+        if (!exists)
+        {
+            return null;
+        }
+
+        using var http = new HttpClient();
+        var response = http.PostAsJsonAsync("https://localhost:7274/api/accounts", model).Result;
+    }
+}
